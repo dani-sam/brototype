635500433210364b6a943ef762f33d0a06ad1aa9
dani-sam <samdani295@gmail.com>
2023-11-11 12:16:39 +0530

    practice(basic concept of ORM)

--- python django/movie_manager/movie/views.py
+++ python django/movie_manager/movie/views.py
@@ -1,58 +1,22 @@
 from django.shortcuts import render
+from . models import MovieInfo
 
 # Create your views here.
 
 def create(request):
     if request.POST:
-        print(request.POST.get('title'))
+        title = request.POST.get('title')
+        year = request.POST.get('year')
+        desc = request.POST.get('summary')
+
+        movie_obj = MovieInfo(title = title,year = year,description = desc)
+        movie_obj.save()
+
     return render(request,'create.html')
 def list(request):
     
-    movieData={ 
-        'movies' : [{
-        'Title':'underworld',
-        'Summary':'Story of underworld movie',
-        'Year':2018,
-        'success':True,
-        'img':'underworld.jpg'
-    },
-        {
-        'Title':'underdog',
-        'Summary':'Story of underworld movie',
-        'Year':2010,
-        'success':False,
-        'img':'godfather.jpg'
-        },
-        {
-        'Title':'pets',
-        'Summary':'Story of underworld movie',
-        'Year':2021,
-        'success':True,
-        'img':'godfather.jpg'
-        },
-        {
-        'Title':'richice world',
-        'Summary':'Story of underworld movie',
-        'Year':2008,
-        'success':False,
-        'img':'underworld.jpg',
-
-        },
-        {
-        'Title':'moz and cat',
-        'Summary':'Story of underworld movie',
-        'Year':2000,
-        'success':False,
-        'img':'underworld.jpg'
-        },
-        {
-        'Title':'Godfather',
-        'Summary':'Story of underworld movie',
-        'Year':2001,
-        'success':True,
-        'img':'godfather.jpg'
-        },
-        ]}
-    return render(request,'list.html',movieData)
+    movie_set = MovieInfo.objects.all() 
+    print(movie_set)
+    return render(request,'list.html',{'movies':movie_set})
 def edit(request):
     return render(request,'edit.html')
